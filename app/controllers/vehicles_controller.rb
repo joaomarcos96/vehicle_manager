c815c8da06a7339e020a297826cbd41c6870d512
Joao Marcos <jao.markos@hotmail.com>
2020-10-29 20:37:56 -0300

    Add Vehicles controller

---
+++ app/controllers/vehicles_controller.rb
@@ -0,0 +1,45 @@
+class VehiclesController < ApplicationController
+  before_action :set_vehicle, only: [:show, :update, :destroy]
+
+  def index
+    @vehicles = Vehicle.all
+
+    render json: @vehicles
+  end
+
+  def show
+    render json: @vehicle
+  end
+
+  def create
+    @vehicle = Vehicle.new(vehicle_params)
+
+    if @vehicle.save
+      render json: @vehicle, status: :created
+    else
+      render json: @vehicle.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @vehicle.update(vehicle_params)
+      render json: @vehicle
+    else
+      render json: @vehicle.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @vehicle.destroy
+  end
+
+  private
+
+    def set_vehicle
+      @vehicle = Vehicle.find(params[:id])
+    end
+
+    def vehicle_params
+      params.require(:vehicle).permit(:make, :model, :year, :description, :sold)
+    end
+end
